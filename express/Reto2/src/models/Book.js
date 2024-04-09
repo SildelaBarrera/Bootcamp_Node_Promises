6dc3175d2ad8b95140caeb7e6cd10ba8ba4581e4
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-04-09 13:10:49 +0200

    commit pokeapi

---
+++ express/Reto2/src/models/Book.js
@@ -0,0 +1,12 @@
+
+class Book{
+
+    constructor(title, author, numPages){
+
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+    }
+}
+
+module.exports = {Book}
